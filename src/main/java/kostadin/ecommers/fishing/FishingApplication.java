138e977182ee63ca3aaac8cac7f6930fe30fbf26
kocem <kocem@DESKTOP-VDMPBQO>
2020-12-02 12:32:54 +0200

    Add hello world message

--- src/main/java/kostadin/ecommers/fishing/FishingApplication.java
+++ src/main/java/kostadin/ecommers/fishing/FishingApplication.java
@@ -8,6 +8,7 @@ public class FishingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FishingApplication.class, args);
+        System.out.println("Hello world");
     }
 
 }
